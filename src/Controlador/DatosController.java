241d09d211972a9d996364cc7c741edcb21bb537
Juan <Juan@10.0.4.210>
2021-08-13 08:54:18 -0600

    Final del programa

--- src/Controlador/DatosController.java
+++ src/Controlador/DatosController.java
@@ -1,7 +1,23 @@
 
 package Controlador;
-
+import Modelo.DatosBean;
+import java.util.Scanner;
 
 public class DatosController {
+
+    private static String nombre;
+    private static int edad;
+    
+    public static void main(String[] args) {
+        Scanner info = new Scanner(System.in);
+        
+        System.out.println("Por favor, ingrese su nombre: ");
+        nombre = info.nextLine();
+        System.out.println("Ingrese su edad: ");
+        edad = info.nextInt();
+        
+        System.out.println("Su nombre es: "+ nombre);
+        System.out.println("Su edad es: " + edad);
+    }
     
 }
